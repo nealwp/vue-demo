0c3d16c4a096e8f632f16968ef19a6a08e2ef1d3
wprestonneal <mrprestonneal@gmail.com>
2022-01-27 12:49:03 -0800

    greeting app

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <p v-if="isMorning">Good Morning!</p>
+    <p v-if="isAfternoon">Good Afternoon!</p>
+    <p v-if="isEvening">Good Evening!</p>
   </div>
 </template>
 
 <script>
+var hours = new Date().getHours();
+
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      isMorning: hours < 12,
+      isAfternoon: hours >= 12 && hours < 18,
+      isEvening: hours >= 18
+    }
+  }
 }
 </script>
 
